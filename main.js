1b5f76e4d328c344d23cfedfc0ae5e5b4942a7f1
Manny Dador <EADador@gmail.com>
2022-04-01 17:49:30 -0400

    Made sep visuals.js

--- main.js
+++ main.js
@@ -1,61 +1,23 @@
-console.log ("Java Connected?!")
+console.log ("Java 2 Connected?!")
 
-//from Thinkific - What can JS do with the DOM
-// 1. Set variables
-const clr = document.getElementById("clear");
-// const output = document.getElementById("output");
-// const seven = document.getElementById("seven");
-// const eight = document.getElementById("eight");
-// const nine = document.getElementById("nine");
-const divi = document.getElementById("dOperator");
-// const four = document.getElementById("four");
-// const five = document.getElementById("five");
-// const six = document.getElementById("six");
-// const multiply = document.getElementById("multiply");
-// const one = document.getElementById("one");
-// const two = document.getElementById("two");
-// const three = document.getElementById("three");
-// const subtract = document.getElementById("subtract");
-// const zeroe = document.getElementById("div3");
-// const decimal = document.getElementById("div4");
-// const equals = document.getElementById("div5");
-// const plus = document.getElementById("plus");
+// Create results and look for ID output
+const outcome = document.getElementById("output")
+// Print text outcomes div and what ID is found
+console.log("The Outc Div", outcome); 
 
-
-// 2. Set event listener
-clr.addEventListener("mouseover", changeGreen);
-clr.addEventListener("mouseout", resetStyle);
-
-// 2a. testing for divide text color change
-divi.addEventListener("mouseover", changeWhite);
-divi.addEventListener("mouseout", resetStyle);
-
-
-// 3. Event (? my explanation) 
-// Change background color to green
-function changeGreen() {
-    clr.style.backgroundColor = "green";
-  }
-
-  // 3. Event (? my explanation)
-  // Reset background color
-  function resetStyle(e) {
-    e.target.style.backgroundColor = "";
-    e.target.style.color = "";
-  }
-
-//3a. Divide aka divi color change from black to white
-function changeWhite() {
-divi.style.color = "white";
+// Create squares and look for all classes containing the
+// name square
+let squares = document.querySelectorAll('.square')
+// Create another array square via the existing array squares
+for (let square of squares){
+// using that new array, JS for listen for click and the function 
+// event ? -> unsure about this.. but it already exists in JS ?
+    square.addEventListener("click", function (event) {
+// //print what’s in the new array once something is clicked
+        console.log(square)
+//print event? Target (defines where?) innerText (JS module?)
+        console.log(event.target.innerText)
+})
 }
 
-// 3a. don't seem to need this.. redundant perhaps
-// divide resetStyle(e) {
-//   e.target.style.fontcolor = "";
-//   e.target.style.color = "";
-
-
-
-//numbers won't change
-//operators C and = will change to a lighter green
-//other operators invert color - eg black to white
+// event.target.innerText = "click" -> this is my comment: for later
